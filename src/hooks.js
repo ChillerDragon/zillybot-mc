ed84faf2da9047d5ceb8a7829371659e8f39cf11
ChillerDragon <ChillerDragon@gmail.com>
2022-09-21 18:11:57 +0200

    Add the most messy HACKER tag support
    closed #9

--- src/hooks.js
+++ src/hooks.js
@@ -3,14 +3,18 @@ const helpers = require('./helpers')
 const chatCommands = require('./chat_commands')
 const ircBridge = require('./irc_bridge')
 
+const onChatMessage = (zillyBot, username, message) => {
+  logger.log('chat', `<${username}> ${message}`)
+  ircBridge.onMessage(zillyBot, username, message)
+  if (username === zillyBot.mc.username) return
+  if (message === 'chat test') zillyBot.mcChatDropSpam('uwu test')
+
+  chatCommands.onMessage(zillyBot, username, message)
+}
+
 const initMcHooks = (zillyBot) => {
   zillyBot.mc.on('chat', (username, message) => {
-    logger.log('chat', `<${username}> ${message}`)
-    ircBridge.onMessage(zillyBot, username, message)
-    if (username === zillyBot.mc.username) return
-    if (message === 'chat test') zillyBot.mcChatDropSpam('uwu test')
-
-    chatCommands.onMessage(zillyBot, username, message)
+    onChatMessage(zillyBot, username, message)
   })
 
   zillyBot.mc.on('whisper', (username, message) => {
@@ -18,9 +22,22 @@ const initMcHooks = (zillyBot) => {
     zillyBot.mcChatDropSpam(`/tell ${username} I am a bot. My code is here: <https://github.com/ChillerDragon/zillybot-mc>`)
   })
 
-  // zillyBot.mc.on('message', (message) => {
-  //   console.log(message)
-  // })
+  zillyBot.mc.on('message', (message) => {
+    // console.log(message)
+
+    // support <HACKER | nametag>
+    if (message.translate === 'chat.type.text') {
+      if (message.with.length === 2) {
+        if (message.with[0].text === 'HACKER' && message.with[0].extra) {
+          if (message.with[0].extra.length === 2) {
+            const username = message.with[0].extra[1]
+            const chatMessage = message.with[1].text
+            onChatMessage(zillyBot, username, chatMessage)
+          }
+        }
+      }
+    }
+  })
 
   zillyBot.mc.on('messagestr', (messagestr, _position, message) => {
     if (message.translate === 'chat.type.text') return
